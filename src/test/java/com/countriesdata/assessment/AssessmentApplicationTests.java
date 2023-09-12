7dbee7a716bd7e020451e46457ae04c01c61756a
Tunde <tunde4excel@gmail.com>
2023-09-12 22:24:08 +0100

    file setup

---
+++ src/test/java/com/countriesdata/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.countriesdata.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
