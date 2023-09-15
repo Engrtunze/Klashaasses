1d00dea572b7f6e7a56b98615d5e3910861cc0ab
Tunde <tunde4excel@gmail.com>
2023-09-15 17:13:10 +0100

    coverted country currency to targeted currency

--- src/main/java/com/countriesdata/assessment/service/CountryService.java
+++ src/main/java/com/countriesdata/assessment/service/CountryService.java
@@ -1,7 +1,10 @@
 package com.countriesdata.assessment.service;
 
+import com.countriesdata.assessment.dto.CountryCurrencyConversionDTO;
+import com.countriesdata.assessment.dto.CountryCurrencyConversionRequest;
 import com.countriesdata.assessment.dto.CountryGeneralDetailsDTO;
 
 public interface CountryService {
 CountryGeneralDetailsDTO getCountryDetails(String country);
+CountryCurrencyConversionDTO covertCurrency(CountryCurrencyConversionRequest request);
 }
